57421a93a7f1f89b0e2aacceafafe2a5e033944e
Miguel <69588669+Miguel190751@users.noreply.github.com>
2021-03-16 00:19:26 +0900

    Update design(main.php)

--- Main/main.php
+++ Main/main.php
@@ -17,13 +17,22 @@ if(!($_SESSION['login_name'])){
 <head>
     <meta charset="utf-8">
     <title>メインページ</title>
+    <link rel="stylesheet" href="../src/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 </head>
 
 <body>
-    <h1>ようこそ！ <?PHP echo $_SESSION['login_name']; ?> さん</h1>
 
-    <a href="Ja-En_Quiz.php">英->日モード</a>
-    <a href="management.php">例文登録</a>
+    <div class="container">
+    <h1 class="mt-5 mb-5">ようこそ！ <?PHP echo $_SESSION['login_name']; ?> さん</h1>
+
+    <div id="contents"> 
+    <a href="Ja-En_Quiz.php"><img src="../src/pic/pic_JE.png"></a><br>
+    <a href="management.php"><img src="../src/pic/pic_register.png"></a><br>
+    </div>
+
+    </div>
+
 </body>
     
 </html>
