60bfe814510bc1ad6e324ca2ab78ead43a19f42c
Miguel <69588669+Miguel190751@users.noreply.github.com>
2021-04-02 14:47:30 +0900

    Update Ja-En_Quiz.php
    Add Ajax

--- Main/Ja-En_Quiz.php
+++ Main/Ja-En_Quiz.php
@@ -7,17 +7,6 @@ session_start();
 //DB接続
 $pdo = new PDO(DB_NAME,DB_USER,DB_PASSWD,array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
 
-//変数の未定義を防ぐための定義
-$question = "";
-$answer = "";
-$en_hint1 = "";
-$ja_hint1 = "";
-$en_hint2 = "";
-$ja_hint2 = "";
-$en_hint3 = "";
-$ja_hint3 = "";
-$selected_id = 0;
-
 //セッション情報の確認
 if(!($_SESSION['login_name'])){
   header('Location: login.php');
@@ -29,48 +18,16 @@ if($_SESSION['num'] > 5){
   header('Location: main.php');
 }
 
-//問題表示・正解・不正解ボタンが押された場合の処理。
+//正解・不正解ボタンが押された場合の処理。
 if($_SERVER['REQUEST_METHOD'] === 'POST'){
-
-  //問題表示処理
-  if(!empty($_POST['question'])){
-
-    //問題をランダム選択する処理
-    $sql = 'SELECT sentence_id FROM exsentence WHERE user_id=:user_id';
-    $stmt = $pdo->prepare($sql);
-    $stmt->bindParam(':user_id',$user_id,PDO::PARAM_INT);
-    $user_id =  $_SESSION['login_id'];
-    $stmt->execute();
-    $results = $stmt->fetchAll();
-    $x = count($results);
-    $random = rand(0, $x-1);
-    $selected_id = $results[$random][0];
-
-    $sql = 'SELECT * FROM exsentence where sentence_id=:sentence_id AND user_id=:user_id';
-    $stmt = $pdo -> prepare($sql);
-    $stmt->bindParam(':sentence_id',$selected_id,PDO::PARAM_INT);
-    $stmt->bindParam(':user_id',$user_id,PDO::PARAM_INT);
-    $stmt->execute();
-    $results = $stmt->fetchAll();
-    foreach ($results as $row){
-      $question = $row['ensentence'];
-      $answer = $row['jasentence'];
-      $en_hint1 = $row['enkeyword1'];
-      $ja_hint1 = $row['jakeyword1'];
-      $en_hint2 = $row['enkeyword2'];
-      $ja_hint2 = $row['jakeyword2'];
-      $en_hint3 = $row['enkeyword3'];
-      $ja_hint3 = $row['jakeyword3'];
-    }
-  }
     
   //正解していれば正解数に+1、外れればそのまま。+ 次の問題に遷移する(ページ更新)処理
-  if(!empty($_POST['correct']) && $_POST['id']){
+  if(!empty($_POST['correct']) && !empty($_POST['selected_id'])){
 
       //現在の出題数に+1する処理
       $_SESSION['num'] += 1;
 
-      $sentence_id = $_POST['id'];
+      $sentence_id = $_POST['selected_id'];
       $user_id =  $_SESSION['login_id'];
 
       //正解数(correct)を取得する処理。
@@ -98,15 +55,14 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
       header("Location: " . $_SERVER['PHP_SELF']);
   }
 
-  if(!empty($_POST['incorrect']) && $_POST['id']){
-    
+  if(!empty($_POST['incorrect']) && !empty($_POST['selected_id'])){
+
       //現在の出題数に+1する処理
       $_SESSION['num'] += 1;
 
       //ページを更新する。(新しい問題に切り替え)
       header("Location: " . $_SERVER['PHP_SELF']);
-  }   
-    
+  }    
 }
 
 ?>
@@ -127,30 +83,28 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
 
     <h1> <?php echo '第'.$_SESSION['num'].'問' ?></h1>
 
-    <form action="" method="post">
-      <input class="quizbtn btn btn-primary mb-2" type="submit" name="question" value="問題を表示">
-      <div id="question"><?php echo $question; ?></div>
-    </form>
+    <button id="questionbtn" class="quizbtn btn btn-primary mb-2" onClick="get_question()">問題を表示</button>
+    <div id="question"></div>
 
     <br><br> 
 
-    <button id="hintbtn" class="quizbtn btn btn-primary mb-2">ヒント</button><br>
+    <button id="hintbtn" class="quizbtn btn btn-danger mb-2">ヒント</button><br>
     <div id="hint">
       <table>
         <table border='1' align="center">
           <tr id="hintdisplay1">
-            <td><?php echo $en_hint1 ?></td>
-            <td><?php echo $ja_hint1 ?></td>
+            <td id="en_hint1"></td>
+            <td id="ja_hint1"></td>
           </tr>
 
           <tr id="hintdisplay2">
-            <td><?php echo $en_hint2 ?></td>
-            <td><?php echo $ja_hint2 ?></td>
+            <td id="en_hint2"></td>
+            <td id="ja_hint2"></td>
           </tr>
 
           <tr id="hintdisplay3">
-            <td><?php echo $en_hint3 ?></td>
-            <td><?php echo $ja_hint3 ?></td>
+            <td id="en_hint3"></td>
+            <td id="ja_hint3"></td>
           </tr>
       </table>
     </div>
@@ -159,15 +113,16 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
 
 
     <button id="answerbtn" class="quizbtn btn btn-primary mb-2">解答を表示</button>
-    <div id="answer"><?php echo $answer; ?></div>
+    <div id="answer"></div>
 
     <br><br>
 
     <form action="" method="post">
       <input class="quizbtn btn btn-success" id="correct" type="submit" name="correct" value="正解！">
       <input class="quizbtn btn btn-success" type="submit" name="incorrect" value="不正解！">
-      <input type="hidden" name="id" value= "<?PHP echo $selected_id; ?>">
+      <input type="hidden" name="selected_id" id="selected_id">
     </form>
+
     <br>
 
     <a href="main.php">メインページに戻る</a>
@@ -175,6 +130,33 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
   </div>
 
   <script>
+    let request = new XMLHttpRequest();
+
+    request.onreadystatechange = function(){
+      if(request.readyState == 4){
+        if(request.status == 200){
+          //JSONファイルを受取
+          let responseJson = JSON.parse(request.response);
+          //console.log(responseJson);
+          document.getElementById('question').textContent = responseJson.ensentence;
+          document.getElementById('answer').textContent = responseJson.jasentence;
+          document.getElementById('en_hint1').textContent = responseJson.enkeyword1;
+          document.getElementById('ja_hint1').textContent = responseJson.jakeyword1;
+          document.getElementById('en_hint2').textContent = responseJson.enkeyword2;
+          document.getElementById('ja_hint2').textContent = responseJson.jakeyword2;
+          document.getElementById('en_hint3').textContent = responseJson.enkeyword3;
+          document.getElementById('ja_hint3').textContent = responseJson.jakeyword3;
+          document.getElementById('selected_id').value = responseJson.sentence_id;
+        }
+      }
+    }
+
+    function get_question(){
+    request.open('POST', 'http://localhost/Works/ES_App/src/select_question.php');
+    request.send(null);
+    document.getElementById('questionbtn').style.display = 'none';
+    }
+
     //解答を表示が押された場合に非表示から表示に変更する。
     document.getElementById('answer').style.display = "none";
 
@@ -203,7 +185,8 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
         }
         //正解！ボタンを押せないように変更する。
         document.getElementById("correct").setAttribute("disabled", true);
-        })
+        }
+      )
   </script>
 </body>
     
---
+++ src/select_question.php
@@ -0,0 +1,33 @@
+<?php
+
+require_once('config.php');
+
+session_start();
+
+//DB接続
+$pdo = new PDO(DB_NAME,DB_USER,DB_PASSWD,array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
+
+$sql = 'SELECT sentence_id FROM exsentence WHERE user_id=:user_id';
+$stmt = $pdo->prepare($sql);
+$stmt->bindParam(':user_id',$user_id,PDO::PARAM_INT);
+$user_id =  $_SESSION['login_id'];
+$stmt->execute();
+$results = $stmt->fetchAll();
+$x = count($results);
+$random = rand(0, $x-1);
+$selected_id = $results[$random][0];
+
+$sql = 'SELECT * FROM exsentence where sentence_id=:sentence_id AND user_id=:user_id';
+$stmt = $pdo -> prepare($sql);
+$stmt->bindParam(':sentence_id',$selected_id,PDO::PARAM_INT);
+$stmt->bindParam(':user_id',$user_id,PDO::PARAM_INT);
+$stmt->execute();
+$results = $stmt->fetchAll();
+foreach ($results as $row){
+  $list = array("sentence_id" => $row['sentence_id'], "ensentence" => $row['ensentence'], "jasentence" => $row['jasentence'], "enkeyword1" => $row['enkeyword1'], "jakeyword1" => $row['jakeyword1'], "enkeyword2" => $row['enkeyword2'], "jakeyword2" => $row['jakeyword2'], "enkeyword3" => $row['enkeyword3'], "jakeyword3" => $row['jakeyword3']);
+}
+
+echo json_encode($list);
+exit();
+
+?>
