8a5bc923480221334d0ab32526a827929ad25509
Miguel <69588669+Miguel190751@users.noreply.github.com>
2021-04-06 12:20:05 +0900

    add program to result.php

--- Main/result.php
+++ Main/result.php
@@ -1,3 +1,45 @@
+<?php
+
+require_once('../src/config.php');
+
+session_start();
+
+//DB接続
+$pdo = new PDO(DB_NAME,DB_USER,DB_PASSWD,array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
+
+if(!($_SESSION['login_name'])){
+  header('Location: login.php');
+}
+
+if(!empty($_SESSION['selected_id1']) && !empty($_SESSION['selected_id2']) && !empty($_SESSION['selected_id3']) && !empty($_SESSION['selected_id4']) && !empty($_SESSION['selected_id5']) ){
+
+  //*配列の無駄が多いのでSQLの修正必要。 *SESSIONの無駄も多い。
+  for($i=1;$i<=5;$i++){
+    $index = 'selected_id'.$i;
+
+    $selected_id = $_SESSION[$index];
+    $user_id = $_SESSION['login_id'];
+  
+    $sql = "SELECT * FROM exsentence WHERE sentence_id=:sentence_id AND user_id=:user_id";
+    $stmt = $pdo -> prepare($sql);
+    $stmt->bindParam(':sentence_id',$selected_id,PDO::PARAM_INT);
+    $stmt->bindParam(':user_id',$user_id,PDO::PARAM_INT);
+    $stmt->execute();
+    $results[$i] = $stmt->fetchAll();
+
+    /*
+    echo('<pre>');
+    var_dump($results);
+    echo('</pre>');
+    */
+
+  }
+}else{
+  exit('Error!!');
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -8,6 +50,19 @@
 </head>
 
 <body>
-  <!--セッションで保存したデータ(問題IDと正解/不正解かの2つを保存すれば可(５問文))を表示すればよさそう/セッションの勉強もしとくべし-->  
+  <div class="container mt-3">
+      <p>
+        <?php
+        for($i=1;$i<=5;$i++){
+          echo $results[$i][0]['ensentence'] . "<br>";
+          echo $results[$i][0]['jasentence'] . "<br>";
+          echo $_SESSION['result'.$i] . "<br>"; 
+        }
+        ?>
+      </p>
+
+      <a href="main.php">メインページに戻る</a>  
+  </div>
+
 </body>
 </html>
